7f3521a04ec58454c0e363c74a4ad33ea1cf75bd
mac <>
2018-07-21 12:10:18 +0700

    Flutter buoi 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,59 @@ class RandomEnglishWords extends StatefulWidget{
 }
 // state
 class RandomEnglishWordsState extends State<RandomEnglishWords>{
+  final _words=<WordPair>[];
+  final _checkedWords=new Set<WordPair>(); // set contains "no duplicate items"
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     final wordPair=new WordPair.random();
-    return new Text(
-      wordPair.asUpperCase,
-      style: new TextStyle(fontSize: 20.0),
-    );
+//    return new Text(
+//      wordPair.asUpperCase,
+//      style: new TextStyle(fontSize: 20.0),
+//    );
+
+  return new Scaffold(
+    appBar: new AppBar(
+      title: new Text("List of English words"),
+    ),
+    body: new ListView.builder(itemBuilder: (context, index){
+
+      if(index>=_words.length){
+        _words.addAll(generateWordPairs().take(10));
+      }
+
+      return _buildRow(_words[index], index);
+    }),
+  );
 
   }
+
+  Widget _buildRow(WordPair wordPair, int index){
+
+    final color=index % 2 ==0 ? Colors.red : Colors.green;
+    final ischecked=_checkedWords.contains(wordPair);
+    // this widget is fot each row
+    return new ListTile(
+      leading: new Icon(ischecked ? Icons.check_box:Icons.check_box_outline_blank,
+        color: color,
+
+      ),
+      title: new Text(
+        wordPair.asUpperCase,
+        style: new TextStyle(fontSize: 18.0, color: color),
+      ),
+      onTap: (){
+        setState(() {
+          if(ischecked){
+            _checkedWords.remove(wordPair);
+          }else{
+            _checkedWords.add(wordPair);
+          }
+        });
+      },
+    );
+  }
+
 }
 
 class MyApp extends StatelessWidget{
@@ -32,15 +75,16 @@ class MyApp extends StatelessWidget{
     // TODO: implement build
     return new MaterialApp(
       title: "This is my first Flutter",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("This is Header's title"),
-        ),
-        body: new Center(
-          //child: new Text("This is the body's center"),
-          child:new RandomEnglishWords()
-        ),
-      ),
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text("This is Header's title"),
+//        ),
+//        body: new Center(
+//          //child: new Text("This is the body's center"),
+//          child:new RandomEnglishWords()
+//        ),
+//      ),
+    home: new RandomEnglishWords(),
     );
   }
 }
